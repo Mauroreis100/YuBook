5a7456a33ec60c1dd2b0ec2fe21a58dc90c037c9
Alak4y <4l4k4y@gmail.com>
2025-05-13 12:57:28 +0200

    added google sign in

--- yubook/lib/auth/auth.dart
+++ yubook/lib/auth/auth.dart
@@ -8,6 +8,7 @@ class AuthPage extends StatelessWidget{
 
   const AuthPage({super.key});
 
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       body: StreamBuilder(
--- yubook/lib/components/MyTextField.dart
+++ yubook/lib/components/MyTextField.dart
@@ -1,29 +1,53 @@
 import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget{
-
+class MyTextField extends StatefulWidget {
   final String hintText;
   final bool obscureText;
   final TextEditingController controller;
 
-
-  const MyTextField({super.key,
+  const MyTextField({
+    super.key,
     required this.hintText,
     required this.controller,
     required this.obscureText,
   });
 
-  Widget build(BuildContext context){
+  @override
+  State<MyTextField> createState() => _MyTextFieldState();
+}
+
+class _MyTextFieldState extends State<MyTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
-      obscureText: obscureText,
+      controller: widget.controller,
+      obscureText: _isObscured,
       decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12)
-        ),
-        hintText: hintText
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        hintText: widget.hintText,
+        suffixIcon:
+            widget.obscureText
+                ? IconButton(
+                  icon: Icon(
+                    _isObscured ? Icons.visibility_off : Icons.visibility,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isObscured = !_isObscured;
+                    });
+                  },
+                )
+                : null,
       ),
     );
   }
-
 }
